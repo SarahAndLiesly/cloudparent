08f0113f8e481b332c8695e76ac601ff9afadccc
wanghui <wangh@ciicsh.com>
2017-12-18 15:05:35 +0800

    配置中心、swagger

--- services/consumer1/src/main/java/com/wh/consumer1/controller/Consumer.java
+++ services/consumer1/src/main/java/com/wh/consumer1/controller/Consumer.java
@@ -1,7 +1,11 @@
 package com.wh.consumer1.controller;
 
 import com.wh.producer1.RemoteProducer1;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/consumer1")
+@RefreshScope
 public class Consumer {
 
     @Autowired
     RemoteProducer1 remoteProducer1;
 
     @GetMapping("/{id}")
-    public Integer getId(@PathVariable Integer id) {
+    @ApiOperation(value = "测试服务调用", notes = "服务调用及降级")
+    public Integer getId(@ApiParam(value = "随便", name = "any", required = true) @PathVariable Integer id) {
         return remoteProducer1.getId(id);
+    }
+
+    @Value("${var}")
+    private String var;
+
+    @ApiOperation(value = "测试配置中心", notes = "获取配置中心配置信息")
+    @GetMapping("/config")
+    public String getConfig() {
+        return var;
     }
 }
